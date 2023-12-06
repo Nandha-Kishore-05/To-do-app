1860e4d2c10e91bad8d4134b0145c4ad5c282d57
Nandha-Kishore-05 <nandhakishore.offical@gmail.com>
2023-12-06 15:39:32 +0530

    finally completed

---
+++ script.js
@@ -0,0 +1,40 @@
+// function add(){
+//     var text = document.getElementById("text");
+//     var task = document.getElementById("task");
+
+//     if(text.value !=""){
+//         var li = document.createElement("li");
+//         li.innerHTML = '<input type="checkbox" class="custom-checkbox"></input>' + text.value + '<button class="button2" onclick="DeleteTask(this)">Delete</button>';
+//         task.append (li);
+//         text.value = "";
+//     }else {
+//         alert("Please enter a task!");
+//       }
+// }
+// function DeleteTask() {
+//     task.remove(); 
+//   }
+function add() {
+    var text = document.getElementById("text");
+    var taskList = document.getElementById("task");
+
+    if (text.value !== "") {
+        var li = document.createElement("li");
+        li.innerHTML =
+            '<input type="checkbox" class="custom-checkbox">' +
+            text.value +
+            '<button class="button2" onclick="deleteTask(this)">Delete</button>';
+        taskList.appendChild(li);
+        text.value = "";
+    } else {
+        alert("Please enter a task!");
+    }
+}
+
+function deleteTask(taskElement) {
+    var listItem = taskElement.parentNode;
+    var taskList = listItem.parentNode;
+    taskList.removeChild(listItem);
+}
+
+
